8d30d4d77952ad488f1b3d9c02471b3ff169ddcc
Chandu G <chandugandupalli777@gmail.com>
2025-09-26 10:39:42 +0530

    adding 15000

--- backend/server.js
+++ backend/server.js
@@ -91,5 +91,5 @@ app.get("/colleges", async (req, res) => {
 });
 
 // Dynamic port for Render
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 15000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
